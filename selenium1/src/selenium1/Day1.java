be8520e4cbdddd33d6906c691a6ca3b40daa20d9
kvks47002 <kvks47002@gmail.com>
2021-12-27 12:22:48 -0500

    Revert "testing revert command"
    This reverts commit 6571666bf5eed5e7cc2794788d08896a13bcf840.

--- selenium1/src/selenium1/Day1.java
+++ selenium1/src/selenium1/Day1.java
@@ -1,7 +1,5 @@
 package selenium1;
 
-import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,19 +7,15 @@ public class Day1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\v\\Downloads\\chromedriver_win32\\chromedriver.exe");
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\v\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(1));
-		
-		driver.get("https://www.google.com");
-		System.out.println(driver.getTitle());
-		System.out.println(driver.getCurrentUrl());
-		System.out.println(driver.getPageSource());
-		System.out.println(driver.getWindowHandle());
-		System.out.println(driver.getPageSource());
-		System.out.println(driver.getTitle());
+WebDriver driver = new ChromeDriver();
+
+driver.get("https://www.google.com");
+System.out.println(driver.getTitle());
+System.out.println(driver.getCurrentUrl());
+
+System.out.println(driver.getPageSource());
 
 	}
 
